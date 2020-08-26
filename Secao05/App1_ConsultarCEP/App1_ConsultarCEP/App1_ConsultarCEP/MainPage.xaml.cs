89e7793ed387f3274d29913fe4ea039714096aed
carl0sR0ma0 <cvpromao@gmail.com>
2020-08-26 00:39:46 -0300

    App01 - Concluído

---
+++ Secao05/App1_ConsultarCEP/App1_ConsultarCEP/App1_ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App1_ConsultarCEP.Servico.Modelo;
+using App1_ConsultarCEP.Servico;
+
+namespace App1_ConsultarCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Botao.Clicked += buscarCep;
+        }
+
+        private void buscarCep(object sender, EventArgs args)
+        {
+            //TODO - Validações
+            string cep = CEP.Text.Trim();
+
+            if (isValidCEP(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+                    if (end != null)
+                    {
+                        Resultado.Text = string.Format("Endereço: {0}, {1}, {2} - {3}", end.logradouro, end.bairro, end.localidade, end.uf);
+                    }
+                    else
+                    {
+                        DisplayAlert("ERRO", "O endereço não foi encontrado para o CEP informado: " + cep, "OK");
+                    }
+                    
+                }
+                catch (Exception e)
+                {
+                    DisplayAlert("ERRO CRÍTICO", e.Message, "OK");                  
+                }
+            }
+        }
+
+        private bool isValidCEP(string cep)
+        {
+            bool valido = true;
+
+            if (cep.Length != 8)
+            {
+                DisplayAlert("ERRO", "CEP inválido! O CEP deve ter 8 caracteres.", "OK");
+                valido = false;
+            }
+            int novoCEP = 0;
+            if (!int.TryParse(cep, out novoCEP))
+            {
+                DisplayAlert("ERRO", "CEP inválido! O CEP deve ser composto apenas por números.", "OK");
+                valido = false;
+            }
+            return valido;
+        }
+    }
+}
