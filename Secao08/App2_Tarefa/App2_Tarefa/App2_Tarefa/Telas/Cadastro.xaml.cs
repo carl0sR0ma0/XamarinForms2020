48045c4ea8261749db3289f6adb87ff5d9ec71f7
carl0sR0ma0 <cvpromao@gmail.com>
2020-09-08 01:21:15 -0300

    Desenvolvimento do app Daily ToDo, finalização da parte de banco de dados nativo do S.O.

---
+++ Secao08/App2_Tarefa/App2_Tarefa/App2_Tarefa/Telas/Cadastro.xaml.cs
@@ -0,0 +1,70 @@
+﻿using App2_Tarefa.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using App2_Tarefa.Modelos;
+
+namespace App2_Tarefa.Telas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Cadastro : ContentPage
+    {
+        private byte Prioridade { get; set; }
+        public Cadastro()
+        {
+            InitializeComponent();
+        }
+
+        public void PrioridadeSelectAction(object sender, EventArgs args)
+        {
+            var Stacks = SLPrioridades.Children;
+
+            foreach (var Linha in Stacks)
+            {
+                Label LblPrioridade = ((StackLayout)Linha).Children[1] as Label;
+                LblPrioridade.TextColor = Color.Gray;
+            }
+
+            ((Label)((StackLayout)sender).Children[1]).TextColor = Color.Black;
+            FileImageSource Source = ((Image)((StackLayout)sender).Children[0]).Source as FileImageSource;
+
+            String Prioridade = Source.File.ToString().Replace("img", "").Replace(".png", "");
+
+            this.Prioridade = byte.Parse(Prioridade);
+        }
+
+        public void SalvarAction(object sender, EventArgs args)
+        {
+            bool ErroExiste = false;
+
+            if (TxtNome.Text == null || TxtNome.Text.Trim().Length <= 0)
+            {
+                ErroExiste = true;
+                DisplayAlert("ERRO", "Tarefa não digitada!", "OK");
+            }
+
+            if (Prioridade <= 0)            
+            {
+                ErroExiste = true;
+                DisplayAlert("ERRO", "Prioridade não escolhida!", "OK");
+            }
+
+            if (ErroExiste == false)
+            {
+                // Salva esses dados
+                Tarefa tarefa = new Tarefa();
+                tarefa.Nome = TxtNome.Text.Trim();
+                tarefa.Prioridade = this.Prioridade;
+
+                new GerenciadorTarefa().Salvar(tarefa);
+
+                App.Current.MainPage = new NavigationPage(new Inicio());
+            }
+        }
+    }
+}
